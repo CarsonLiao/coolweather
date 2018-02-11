be35698190e3810584134803de05d30eb0e91127
Carson <liaozhihua_916@163.com>
2018-02-11 18:52:28 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.coolweather.android;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -107,6 +108,12 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
@@ -248,7 +255,7 @@ public class ChooseAreaFragment extends Fragment {
         if (progressDialog ==null){
             progressDialog = new ProgressDialog(getActivity());
             progressDialog.setMessage("正在加载...");
-            Toast.makeText(getContext(), "IntentWrong?", Toast.LENGTH_SHORT).show();
+           // Toast.makeText(getContext(), "IntentWrong?", Toast.LENGTH_SHORT).show();
             progressDialog.setCanceledOnTouchOutside(false);
         }
         progressDialog.show();
